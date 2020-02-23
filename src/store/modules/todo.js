5e8a81de8e5f246e442488330fe31fa2680b25d0
qinjialei <jialei.qin@thoughtworks.com>
2020-02-23 18:11:26 +0800

    1

--- src/store/modules/todo.js
+++ src/store/modules/todo.js
@@ -1,32 +1,4 @@
-import { handleActions, getActionMap, createModel } from "../util";
-
-// const initialState = {
-//   inputValue: '123',
-//   list: []
-// }
-// const reducers = {
-//   add(state, action) {
-//     state.list.push(action.data)
-//   },
-//   delete(state, action) {
-//     state.list.splice(action.data, 1)
-//   },
-//   changeInput(state, action) {
-//     state.inputValue = action.data
-//   }
-// }
-
-// const fn = (state = initialState, action) => handleActions({
-//   state,
-//   action,
-//   reducers,
-//   namespace: 'todo'
-// })
-
-// fn.action = getActionMap(reducers, 'todo')
-// export default fn
-
-
+import { createModel } from "../util";
 
 // 定义需要管理的状态
 const model = {
