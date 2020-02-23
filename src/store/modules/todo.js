6a14186898b58fabdba3a6f250a2186e68848028
qinjialei <jialei.qin@thoughtworks.com>
2020-02-23 18:10:49 +0800

    finis createModel api

--- src/store/modules/todo.js
+++ src/store/modules/todo.js
@@ -1,29 +1,60 @@
-import { handleActions, getActionMap } from "../util";
+import { handleActions, getActionMap, createModel } from "../util";
 
-const initialState = {
-  inputValue: '123',
-  list: []
-}
-const reducers = {
-  add(state, action) {
-    state.list.push(action.data)
+// const initialState = {
+//   inputValue: '123',
+//   list: []
+// }
+// const reducers = {
+//   add(state, action) {
+//     state.list.push(action.data)
+//   },
+//   delete(state, action) {
+//     state.list.splice(action.data, 1)
+//   },
+//   changeInput(state, action) {
+//     state.inputValue = action.data
+//   }
+// }
+
+// const fn = (state = initialState, action) => handleActions({
+//   state,
+//   action,
+//   reducers,
+//   namespace: 'todo'
+// })
+
+// fn.action = getActionMap(reducers, 'todo')
+// export default fn
+
+
+
+// 定义需要管理的状态
+const model = {
+  namespace: 'todo',//命名空间
+  state: {
+    inputValue: '123',
+    list: []
   },
-  delete(state, action) {
-    state.list.splice(action.data, 1)
+  reducer: {//操作 state 内的变量，必须是 纯函数 ，不能存放带有异步的代码
+    add(state, action) {
+      state.list.push(action.data)
+    },
+    delete(state, action) {
+      state.list.splice(action.data, 1)
+    },
+    changeInput(state, action) {
+      state.inputValue = action.data
+    },
   },
-  changeInput(state, action) {
-    state.inputValue = action.data
+  effect: {
+    asyncAdd(reducer, payload) {
+      setTimeout(() => {
+        reducer.add({
+          payload: '222'
+        })
+      }, 1000);
+    }
   }
 }
 
-const fn = (state = initialState, action) => handleActions({
-  state,
-  action,
-  reducers,
-  namespace: 'todo'
-})
-
-fn.action = getActionMap(reducers, 'todo')
-console.log("TCL: fn.action", fn.action)
-
-export default fn
+export default createModel(model)
--- src/store/util.js
+++ src/store/util.js
@@ -17,3 +17,28 @@ export const handleActions = ({ state, action, reducers, namespace = '' }) =>
     .includes(action.type)
     ? produce(state, draft => reducers[getKey(action.type, '/')](draft, action))
     : state
+
+export const formatModel = ({ state, action, reducer, namespace = '' }) =>
+  Object.keys(reducer)
+    .map(key => namespace + '/' + key)
+    .includes(action.type)
+    ? produce(state, draft => reducer[getKey(action.type, '/')](draft, action))
+    : state
+
+export const handleEffect = (effect = {}) => {
+
+}
+
+
+export const createModel = (model) => {
+  const { reducer, namespace } = model
+  const fn = (state = model.state, action) => formatModel({
+    state,
+    action,
+    reducer,
+    namespace
+  })
+  // 通过 reducer 的函数名 生成对应的 action，
+  fn.action = getActionMap(reducer, namespace)
+  return fn
+}
